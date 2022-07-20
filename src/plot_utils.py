3ee4e58b8fecf08a8f9377867a8a0a514e8557e4
Giuliano Cornacchia <37023673+GiulianoCornacchia@users.noreply.github.com>
2022-07-21 01:00:32 +0200

    Added demands used in the paper

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -198,3 +198,132 @@ def plot_errorbars(dict__xkey__distribution, plot_mean=True, x_label='', y_label
 ####
 
 
+def streetDraw(place, G, attribute_to_plot, normalize_by_length=False, show_legend=True, legend_title='', schemeColor='yelloworangered', reverse_schemeColor=False, scaleType = 'symlog', size=(600,600), save_fig=False):
+    """ Plots a road network with the roads colored with respect to an attribute.
+
+    Parameters
+    ----------
+    place : str
+        the name of the place that is being plotted (only used for saving).
+
+    G : networkx.MultiDiGraph
+        the road network to be plotted.
+
+    attribute_to_plot : str
+        the name of the feature to be plotted. It must be present as an attribute of the graph's edges.
+    
+    normalize_by_length : boolean
+        whether to normalize the value of the attribute with the edge's length.
+    
+    show_legend : boolean
+        whether to show the legend (colorbar) or not.
+
+    schemeColor : str
+        the name of a Vega's color scheme (https://vega.github.io/vega/docs/schemes/)
+
+    scaleType : str
+        type of Vega scale for altair.Scale (continuous scales: 'linear', 'log', 'pow', 'sqrt', 'symlog', 'time', 'utc', 'sequential').
+        More info here: https://vega.github.io/vega/docs/scales/
+
+    size : tuple
+        the size of the chart as (widthCanvas, heightCanvas).
+
+    save_fig
+        whether to save (in .svg and .png) the output chart or not.
+
+    Returns
+        the Altair chart.
+    -------
+
+    Credits to Daniele Fadda.
+    
+    
+    """
+    
+    import osmnx as ox
+    import pandas as pd
+    import altair as alt
+    from altair_saver import save
+    alt.data_transformers.disable_max_rows()
+
+    widthCanvas = size[0]
+    heightCanvas = size[1]
+
+    edges = ox.graph_to_gdfs(G, nodes=False, node_geometry=False)
+    df = edges.reset_index()
+
+    if attribute_to_plot:
+        df.sort_values(by=attribute_to_plot, na_position='first', ascending=True, inplace=True)
+
+        if normalize_by_length:
+            attribute_to_plot_old = attribute_to_plot
+            attribute_to_plot = attribute_to_plot + ' normalized'
+            df[attribute_to_plot] = (df[attribute_to_plot_old]*10e-3) / df['length']
+            if legend_title:
+                plot_label = legend_title
+            else:
+                plot_label = '%s (grams per meter of road)' %attribute_to_plot_old
+        else:
+            if legend_title:
+                plot_label = legend_title
+            else:
+                plot_label = '%s' %attribute_to_plot
+        #
+        first_nonzero = [x for x in abs(df[attribute_to_plot]).dropna().sort_values() if x != 0][0]
+        #
+        if show_legend:
+            legend = alt.Legend(title=legend_title, 
+                                #orient='bottom',
+                                labelAlign='right',
+                                labelFontSize=14,
+                                #labelLimit=0,
+                                titleFontSize=12,
+                                gradientLength=300,
+                                format='.2s')
+        else:
+            legend = None
+        #
+     
+        lines = alt.Chart(df[df[attribute_to_plot]==0]).mark_geoshape(
+            filled=False,
+            strokeWidth=1.5,
+            color='lightgray'
+        )
+
+        speedLines = alt.Chart(df[df[attribute_to_plot]!=0]).mark_geoshape(
+            filled=False,
+            strokeWidth=2.5
+        ).encode(
+            alt.Color(
+                attribute_to_plot + ':Q',
+                legend=legend,
+                scale=alt.Scale(scheme=schemeColor, type=scaleType, constant=first_nonzero, reverse=reverse_schemeColor)
+            )
+        ).properties(
+            width=widthCanvas,
+            height=heightCanvas
+        )
+
+        chart = lines + speedLines
+    
+    else:
+        lines = alt.Chart(df).mark_geoshape(
+            filled=False,
+            strokeWidth=1.5,
+            color='gray'
+        ).properties(
+            width=widthCanvas,
+            height=heightCanvas
+        )
+        chart = lines
+
+    if save_fig:
+        from altair_saver import save
+        city = place.split(',')[0]
+        attribute = attribute_to_plot.replace(' ', '_')
+        save(chart, f'../results/imgs/{city}_map_{attribute}.png', scale_factor=2.0)
+        #save(chart, f'{city}_map_{attribute}.svg')
+        save(chart, f'../results/imgs/{city}_map_{attribute}.pdf')
+        print(f'{city} images saved')
+
+    return chart
